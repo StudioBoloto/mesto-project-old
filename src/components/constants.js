3f1224084885ec0349d1989d38e61bca18c601dc
Arsenii Shvetsov <arseniijab@icloud.com>
2022-12-21 13:34:56 +0300

    first commit sprint 8

--- src/components/constants.js
+++ src/components/constants.js
@@ -1,3 +1,5 @@
+export {initialCards, myConfiguration}
+
 const initialCards = [
     {
         name: 'Архыз',
@@ -24,3 +26,22 @@ const initialCards = [
         link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
     }
 ];
+
+const myConfiguration = {
+    formSelector: '.popup__form',
+    labelSelector: '.popup__label',
+    inputSelector: '.popup__input',
+    submitButtonSelector: '.popup__save-button',
+    openedPopupClass: 'popup_opened',
+    inactiveButtonClass: 'popup__save-button_inactive',
+    addCardButtonSelector: '.popup__save-button-add-card',
+    inputInvalidSelector: 'popup__input_invalid',
+    inputErrorSelector: 'popup__input_error_active',
+    elementsCardSelector: '.elements__card',
+    elementImageSelector: '#element-image',
+    elementsImageSelector: '.elements__image',
+    elementsTitleSelector: '.elements__title',
+    elementsLikeSelector: '.elements__like',
+    elementsLikeActiveClass: 'elements__like_active',
+    deleteButtonSelector: '#delete-button'
+}
---
+++ src/components/index.js
@@ -0,0 +1,42 @@
+import {addCard, editProfile, createCard, cardsContainer} from "./card.js";
+import {toggleButtonState} from "./utils.js";
+import {checkInputValidity} from "./validate.js";
+import {initialCards, myConfiguration} from "./constants.js";
+
+export {myConfiguration, addCard, editProfile};
+
+for (let i = 0; i < initialCards.length; i++) {
+    const cardElement = createCard(initialCards[i].name, initialCards[i].link, myConfiguration);
+    cardsContainer.append(cardElement);
+}
+
+const setEventListeners = (formElement, myConfiguration) => {
+    const inputList = Array.from(formElement.querySelectorAll(myConfiguration.inputSelector));
+    const buttonElements = Array.from(formElement.querySelectorAll(myConfiguration.submitButtonSelector));
+
+    inputList.forEach((inputElement) => {
+        inputElement.addEventListener('input', function () {
+            checkInputValidity(formElement, inputElement, myConfiguration);
+            toggleButtonState(inputList, buttonElements, myConfiguration);
+        });
+    });
+};
+
+const enableValidation = (myConfiguration) => {
+    const formList = Array.from(document.querySelectorAll(myConfiguration.formSelector));
+    formList.forEach((formElement) => {
+        formElement.addEventListener('submit', function (evt) {
+            evt.preventDefault();
+        });
+        setEventListeners(formElement, myConfiguration)
+
+        const fieldSetList = Array.from(formElement.querySelectorAll(myConfiguration.labelSelector));
+
+        fieldSetList.forEach((fieldSet) => {
+            setEventListeners(fieldSet, myConfiguration);
+        });
+
+    });
+};
+
+enableValidation(myConfiguration);
