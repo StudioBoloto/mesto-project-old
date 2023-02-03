0391d73bb22d047a7ca8ebdc0f7a9052ff72346f
Arsenii Shvetsov <arseniijab@icloud.com>
2023-02-03 12:44:05 +0300

    Revert "Merge pull request #7 from StudioBoloto/feat/mesto-update"
    This reverts commit 32258f5cd0ca667be3224b97365e872e5673a9ed, reversing
    changes made to 342f6f92e4e3f2a80373a424b707ac0de7b10551.

--- src/components/index.js
+++ src/components/index.js
@@ -1,99 +1,26 @@
 import '../pages/index.css';
-
-import {Api} from "./api.js";
-import {PopupWithForm} from "./modal.js";
-import {FormValidator} from "./validate.js";
-import {Card} from "./card.js";
-import {UserInfo, Section} from "./utils.js";
+import {createCard, cardsContainer} from "./card.js";
 import {myConfiguration} from "./constants.js";
+import {enableValidation} from "./validate.js"
+import {editProfile, editAvatar} from "./modal.js"
+import {getUserInfo, getInitialCards} from "./api.js";
 
+const allPromise = Promise.all([getUserInfo(), getInitialCards()]);
 
-export const cardTemplate = document.querySelector(myConfiguration.cardTemplateSelector).content;
-const inputUserName = document.querySelector(myConfiguration.editProfileAuthorSelector);
-const inputUserTitle = document.querySelector(myConfiguration.editProfileAboutSelector);
-const popups = document.querySelectorAll(myConfiguration.popupSelector);
-const popupsWithForm = Array.from(popups).filter(popup => popup.querySelector(myConfiguration.formSelector));
+allPromise.then(([userInfo, initialCards]) => {
+    editProfile(userInfo.name, userInfo.about);
+    editAvatar(userInfo.avatar);
+    myConfiguration.id = userInfo._id;
+    console.log(userInfo);
 
-export const api = new Api(myConfiguration.apiConfig);
-export const userInfo = new UserInfo(myConfiguration, api);
-
-const allPromise = Promise.all([userInfo.getUserInfo(), api.getInitialCards()]);
-
-allPromise.then(([userProfile, initialCards]) => {
-    myConfiguration.id = userProfile._id;
-    userInfo.setUserAvatar({link: userProfile.avatar});
-    userInfo.setUserInfo({author: userProfile.name, about: userProfile.about});
-
-    const cardsContainer = new Section({
-        data: initialCards,
-        renderer: (item) => {
-            const card = new Card(item, myConfiguration.elementsCardSelector);
-            const cardElement = card.createCard();
-            cardsContainer.addItem(cardElement);
-        }
-    }, myConfiguration.cardsContainerSelector);
-    cardsContainer.renderItems();
+    initialCards.forEach((initialCard) => {
+        const cardElement = createCard(initialCard.name, initialCard.link, myConfiguration, initialCard);
+        cardsContainer.append(cardElement);
+    });
+    console.log(initialCards);
 })
     .catch((err) => {
         console.log(err);
     });
 
-function addCard(data) {
-    api.pushCard(data)
-        .then((result) => {
-            const cardsContainer = new Section({
-                data: [result],
-                renderer: (item) => {
-                    const card = new Card(item, myConfiguration.elementsCardSelector);
-                    const cardElement = card.createCard();
-                    cardsContainer.addItem(cardElement, true);
-                }
-            }, myConfiguration.cardsContainerSelector);
-            const modalAddCard = new PopupWithForm(myConfiguration.popupAddSelector, () => {
-            });
-            modalAddCard.close();
-            cardsContainer.renderItems();
-            console.log(result);
-        })
-        .catch((err) => {
-            console.log(err);
-        });
-}
-
-popupsWithForm.forEach((popup) => {
-    const form = popup.querySelector(myConfiguration.formSelector)
-    const formValidator = new FormValidator(myConfiguration, form);
-    formValidator.enableValidation();
-    const selector = `#${popup.id}`;
-    const popupInstanceWithForm = (() => {
-        let popupWithForm = null;
-        switch (selector) {
-            case myConfiguration.popupAddSelector:
-                popupWithForm = new PopupWithForm(selector, addCard);
-                document.querySelector(myConfiguration.addButtonSelector).addEventListener('click', function () {
-                    popupWithForm.open();
-                });
-                return popupWithForm;
-            case myConfiguration.popupEditSelector:
-                popupWithForm = new PopupWithForm(selector, userInfo.setUserInfo.bind(userInfo));
-                document.querySelector(myConfiguration.editButtonSelector).addEventListener('click', function () {
-                    inputUserName.value = document.querySelector(myConfiguration.profileAuthorSelector).textContent;
-                    inputUserTitle.value = document.querySelector(myConfiguration.profileAboutSelector).textContent;
-                    popupWithForm.open();
-                });
-                return popupWithForm;
-            case myConfiguration.popupAvatarSelector:
-                popupWithForm = new PopupWithForm(selector, userInfo.setUserAvatar.bind(userInfo));
-                document.querySelector(myConfiguration.avatarSelector).addEventListener('click', function () {
-                    popupWithForm.open();
-                });
-                return popupWithForm;
-            default:
-                popupWithForm = new PopupWithForm(selector, () => {
-                });
-                return popupWithForm;
-        }
-    })();
-
-    popupInstanceWithForm.setEventListeners();
-});
+enableValidation(myConfiguration);
